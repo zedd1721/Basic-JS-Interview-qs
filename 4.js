5c56bbf98090b253b11d72e14f30db35eeb59f1e
Zain Shaikh <szain2024@gmail.com>
2024-01-08 12:19:57 +0530

    Check integer

---
+++ 4.js
@@ -0,0 +1,9 @@
+/* -----------HOW TO CHECK THE NUMBER IS AN INTEGER----------- */
+// Don't use isInteger() method.
+
+let a = 590;
+if(num%1 === 0){
+    console.log("It's an Integer");
+}else{
+    console.log("It's not an Integer");
+}
