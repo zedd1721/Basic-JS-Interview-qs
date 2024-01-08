e904a9893d99c11ec4dfbdb099be5739ec12d716
Zain Shaikh <szain2024@gmail.com>
2024-01-08 11:03:52 +0530

    String Reverse

--- 1.js
+++ 1.js
@@ -8,7 +8,21 @@ let strArr = str.split(" "); //it splits the String on the basis on " " and crea
 
 let updatedArr =  strArr.map(function(word){
     return word.split("").reverse().join("");
+
+    //word.split("") --> it will split the word into array.
+    //EX:- Array ['Hello' , ''World']
+    //on 0th index we have "Hello", it will spilt to array [H,e,l,l,o].
+
+    // .reverse() --> it will reverse the array.
+    //EX:- Array [H,e,l,l,o] --> [o,l,l,e,H]
+
+    // .join("") --> it will join the array to single string without space
+    // EX:- Array [o,l,l,e,H] --> "olleH"
 })
 
+// Now we have reversed array ["olleh", "dlroW"].
+
 console.log(updatedArr.join(" "));
-// console.log();
+// .join(" ") -->it will join the arary with spaces
+// EX:- olleh dlroW
+// NOW WE HAVE REVERSED STRING
